09290fc1ded92bf08f1ecacd45dd43410380bd06
Raghavendra H J <raghavendrahj@Raghavendras-MacBook-Pro.local>
2020-02-12 14:35:27 +0530

    first commit with file

---
+++ App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import IntroductionScreen from "./screens/IntroductionScreen";
+import StartScreen from "./screens/StartScreen";
+import CalendarScreen from "./screens/CalendarScreen";
+import TaskEditorScreen from "./screens/TaskEditorScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import GroupsScreen from "./screens/GroupsScreen";
+import TimelineScreen from "./screens/TimelineScreen";
+import ListScreen from "./screens/ListScreen";
+import OverviewScreen from "./screens/OverviewScreen";
+import SideBar from "./components/SideBar";
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Stack = createStackNavigator();
+
+function StartNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+      <Stack.Screen name="Introduction" component={IntroductionScreen} />
+      <Stack.Screen name="Start" component={MainNavigator} />
+    </Stack.Navigator>
+  );
+}   
+
+const Drawer = createDrawerNavigator();
+
+function MainNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={StartScreen} />
+      <Drawer.Screen name="Calendar" component={CalendarStack} />
+      <Drawer.Screen name="TaskEditor" component={TaskEditorScreen} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Groups" component={GroupsScreen} />
+      <Drawer.Screen name="Timeline" component={TimelineScreen} />
+      <Drawer.Screen name="List" component={ListStack} />
+      <Drawer.Screen name="Overview" component={OverviewScreen} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StartNavigator/>
+    </NavigationContainer>
+  );
+};
